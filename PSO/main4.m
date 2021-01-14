6c4cbc1d252ee0fa6bda8f363fd0f4bae236d526
3ricKim <77449150+3ricKim@users.noreply.github.com>
2021-01-15 00:44:25 +0900

    Update main4.m

--- PSO/main4.m
+++ PSO/main4.m
@@ -9,7 +9,7 @@ tic;
 
 %each row indicates a set
 % mm = [1 5 3 0; 2 1 3 0; 4 0 0 0;1 2 3 5];
-mm=importdata('pumsb.dat');
+mm=importdata('chess.dat'); %import data from chess.dat or mushroom.dat
 
 [gbest,gbestval,cg_curve,sol_best]= PSO_func3(mm,Max_Gen,Max_FES,Particle_Number);
 sol_best            % best sequence of 1/0
